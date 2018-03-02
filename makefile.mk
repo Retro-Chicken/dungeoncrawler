5f5b954b4aad4cfeb3854c50fb6d55d3a55363d1
Will Fisher <retrochicken.dev@gmail.com>
2018-03-02 09:24:57 -0600

    Added windows libraries

--- makefile.mk
+++ makefile.mk
@@ -16,9 +16,17 @@ linux-build: TARGET_PATH := $(TARGET_PATH)linux/
 linux-build: $(TARGET_PATH)main.o $(shell find $(SFML_LINUX_LIB) -type f)
 	$(CXX) $(TARGET_PATH)main.o -o $(TARGET_PATH)$(TARGET) -L$(SFML_LINUX_LIB) $(SFML_ACTIVE_MODULES)
 
-windows-build: TARGET := windows-build
-windows-build: CXX := g++
-windows-build: TARGET_PATH := $(TARGET_PATH)windows/
+windows-build32: TARGET := windows-build
+windows-build32: CXX := g++
+windows-build32: TARGET_PATH := $(TARGET_PATH)windows/
+windows-build32: windows-build
+
+windows-build64: TARGET := windows-build64
+windows-build64: CXX := g++
+windows-build64: SFML_WINDOWS_LIB := sfml/lib-windows64
+windows-build64: TARGET_PATH := $(TARGET_PATH)windows64/
+windows-build64: windows-build
+
 windows-build: $(TARGET_PATH)main.o $(shell find $(SFML_WINDOWS_LIB) -type f)
 	$(CXX) $(TARGET_PATH)main.o -o $(TARGET_PATH)$(TARGET) -L$(SFML_WINDOWS_LIB) $(SFML_ACTIVE_MODULES)
 
@@ -27,7 +35,7 @@ mac-build: CXX := g++
 mac-build: TARGET_PATH := $(TARGET_PATH)mac/
 mac-build: $(TARGET_PATH)main.o
 	$(CXX) $(TARGET_PATH)main.o -o $(TARGET_PATH)$(TARGET) $(SFML_ACTIVE_FRAMEWORKS)
-
+#--------------------------------------- END OF LINKING -------------------------------------------------
 # Compilation commands
 $(TARGET_PATH)main.o: $(SRC_PATH)main.cpp $(shell find $(SFML_INCLUDE) -type f)
 	$(CXX) -c $(SRC_PATH)main.cpp -o $(TARGET_PATH)main.o -I$(SFML_INCLUDE)
