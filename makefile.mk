ce88b07dd7ec1a41e56258744588f78fd5cbcac4
Will Fisher <retrochicken.dev@gmail.com>
2018-03-02 09:39:32 -0600

    Added launcher script

--- makefile.mk
+++ makefile.mk
@@ -25,13 +25,13 @@ windows-build64: TARGET := windows-build64
 windows-build64: CXX := g++
 windows-build64: SFML_WINDOWS_LIB := sfml/lib-windows64
 windows-build64: TARGET_PATH := $(TARGET_PATH)windows64/
-windows-build64: windows-build
+windows-build64: windows-build64
 
 windows-build: $(TARGET_PATH)main.o $(shell find $(SFML_WINDOWS_LIB) -type f)
 	$(CXX) $(TARGET_PATH)main.o -o $(TARGET_PATH)$(TARGET) -L$(SFML_WINDOWS_LIB) $(SFML_ACTIVE_MODULES)
 
 mac-build: TARGET := mac-build
-mac-build: CXX := g++
+mac-build: CXX := clang++
 mac-build: TARGET_PATH := $(TARGET_PATH)mac/
 mac-build: $(TARGET_PATH)main.o
 	$(CXX) $(TARGET_PATH)main.o -o $(TARGET_PATH)$(TARGET) $(SFML_ACTIVE_FRAMEWORKS)
--- run-windows.bat
+++ run-windows.bat
@@ -1,2 +1,2 @@
-mingw32-make -f makefile.mk windows-build
+mingw32-make -f makefile.mk windows-build32
 start bin/windows/windows-build
