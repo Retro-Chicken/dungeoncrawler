999f528eb9a1eaab0b95465adbcaed74c72d7123
Will <retrochicken.dev@gmail.com>
2018-03-12 20:52:06 -0500

    Changed structure to have tiles be basic entities and store drawables

--- src/dungeon/dungeon.cpp
+++ src/dungeon/dungeon.cpp
@@ -6,16 +6,11 @@ const sf::IntRect dungeon::floor::FLOOR = sf::IntRect(0, 64, 16, 16);
 const sf::IntRect dungeon::banner::BANNER = sf::IntRect(288, 128, 16, 32);
 sf::Texture dungeon::tileTexture;
 
-dungeon::tile dungeon::getTile(int x, int y) { return map[y * dungeon::MAP_WIDTH + x]; }
-dungeon::decoration dungeon::getDecoration(int x, int y) { return decorationMap[y * dungeon::MAP_WIDTH + x]; }
+dungeon::tile& dungeon::getTile(int x, int y) { return map[y * dungeon::MAP_WIDTH + x]; }
 
 dungeon::dungeon() {
-	for(int i = 0; i < MAP_WIDTH * MAP_HEIGHT; i++) {
-		tile temp;
-		map.push_back(temp);
-		decoration tempDecoration;
-		decorationMap.push_back(tempDecoration);
-	}
+	for(int i = 0; i < MAP_WIDTH * MAP_HEIGHT; i++)
+		map.push_back(tile(i % MAP_WIDTH, i / MAP_WIDTH));
 	tileTexture.loadFromFile("resources/tilesets/dungeon_tileset.png");
 	generateDungeon();
 }
@@ -30,10 +25,8 @@ void dungeon::update(float deltaTime) {
 
 void dungeon::draw(sf::RenderWindow& window) {
 	for(int x = 0; x < MAP_WIDTH; x++)
-		for(int y = 0; y < MAP_HEIGHT; y++) {
+		for(int y = 0; y < MAP_HEIGHT; y++)
 			getTile(x, y).draw(window);
-			getDecoration(x, y).draw(window);
-		}
 }
 
 void dungeon::generateDungeon() {
@@ -82,15 +75,11 @@ static int fill[] = { 0, 61, 62, 63, 64 };
 //	corridors meet.
 void dungeon::hCorridor(int x1, int x2, int y) {
 	for(int x = std::min(x1, x2); x <= std::max(x1, x2); x++)
-		for(int i = 0; i < CORRIDOR_SIZE; i++)
-			map.at((y - CORRIDOR_SIZE/2 + i) * MAP_WIDTH + x) =
-				dungeon::floor(x, y - CORRIDOR_SIZE/2 + i, fill[rand() % (sizeof(fill)/sizeof(int))]);
+		getTile(x, y).addDrawable(dungeon::floor(fill[rand() % (sizeof(fill)/sizeof(int))]));
 }
 void dungeon::vCorridor(int y1, int y2, int x) {
 	for(int y = std::min(y1, y2); y <= std::max(y1, y2); y++)
-		for(int i = 0; i < CORRIDOR_SIZE; i++)
-			map.at(y * MAP_WIDTH + x - CORRIDOR_SIZE/2 + i) =
-				dungeon::floor(x - CORRIDOR_SIZE/2 + i, y, fill[rand() % (sizeof(fill)/sizeof(int))]);
+		getTile(x, y).addDrawable(dungeon::floor(fill[rand() % (sizeof(fill)/sizeof(int))]));
 }
 void dungeon::corridor(room room1, room room2) {
 	hCorridor(room1.center.x, room2.center.x, room2.center.y);
@@ -112,9 +101,9 @@ void dungeon::tileRoom(room area) {
 		for(int i = area.interior.left; i < area.interior.left + area.interior.width; i++) {
 			if(i == area.interior.left || i == area.interior.left + area.interior.width - 1)
 				for(int j = area.interior.top; j < area.interior.top + area.interior.height; j++)
-					map.at(j * MAP_WIDTH + i) = dungeon::floor(i, j, 69);
+					getTile(i, j).addDrawable(dungeon::floor(69));
 			else
-				map.at(area.interior.top * MAP_WIDTH + i) = dungeon::floor(i, area.interior.top, 69);
+				getTile(i, area.interior.top).addDrawable(dungeon::floor(69));
 		}
 		area.interior.left = area.interior.left + 1;
 		area.interior.width = area.interior.width - 2;
@@ -127,18 +116,18 @@ void dungeon::tileRoom(room area) {
 		for(int j = area.interior.top; j < area.interior.top + area.interior.height; j++) {
 			if(i == area.interior.left) {
 				if(j == area.interior.top)
-					map.at(j * MAP_WIDTH + i) = dungeon::floor(i, j, topLeftCorner[rand() % (sizeof(topLeftCorner)/sizeof(int))]);
+					getTile(i, j).addDrawable(dungeon::floor(topLeftCorner[rand() % (sizeof(topLeftCorner)/sizeof(int))]));
 				else
-					map.at(j * MAP_WIDTH + i) = dungeon::floor(i, j, leftEdge[rand() % (sizeof(leftEdge)/sizeof(int))]);
+					getTile(i, j).addDrawable(dungeon::floor(leftEdge[rand() % (sizeof(leftEdge)/sizeof(int))]));
 			} else if(i == area.interior.left + area.interior.width - 1) {
 				if(j == area.interior.top)
-					map.at(j * MAP_WIDTH + i) = dungeon::floor(i, j, topRightCorner[rand() % (sizeof(topRightCorner)/sizeof(int))]);
+					getTile(i, j).addDrawable(dungeon::floor(topRightCorner[rand() % (sizeof(topRightCorner)/sizeof(int))]));
 				else
-					map.at(j * MAP_WIDTH + i) = dungeon::floor(i, j, rightEdge[rand() % (sizeof(rightEdge)/sizeof(int))]);
+					getTile(i, j).addDrawable(dungeon::floor(rightEdge[rand() % (sizeof(rightEdge)/sizeof(int))]));
 			} else if(j == area.interior.top)
-				map.at(j * MAP_WIDTH + i) = dungeon::floor(i, j, topEdge[rand() % (sizeof(topEdge)/sizeof(int))]);
+				getTile(i, j).addDrawable(dungeon::floor(topEdge[rand() % (sizeof(topEdge)/sizeof(int))]));
 			else
-				map.at(j * MAP_WIDTH + i) = dungeon::floor(i, j, fill[rand() % (sizeof(fill)/sizeof(int))]);
+				getTile(i, j).addDrawable(dungeon::floor(fill[rand() % (sizeof(fill)/sizeof(int))]));
 		}
 }
 
@@ -151,9 +140,8 @@ void dungeon::generateWalls() {
 	int count = 1;
 	for(int y = 0; y < MAP_HEIGHT - 1; y++)
 		for(int x = 0; x < MAP_WIDTH; x++) {
-			if(getTile(x, y).type == tile::EMPTY_T &&
-				getTile(x, y + 1).type == tile::FLOOR_T) {
-				map.at(y * MAP_WIDTH + x) = wall(x, y, count);
+			if(getTile(x, y).isEmpty() && !getTile(x, y + 1).isEmpty()) {
+				getTile(x, y).addDrawable(wall(count));
 				count = 1 + count % 4;
 			} else
 				count = 1;
@@ -165,7 +153,8 @@ void dungeon::decorateRoom(room area) {
 }
 
 void dungeon::generateDecorations() {
-	for(int i = 0; i < map.size(); i++)
-		if(map.at(i).type == tile::WALL_T && rand() % 20 == 0)
-			decorationMap.at(i) = banner(i % MAP_WIDTH, i / MAP_WIDTH, rand() % banner::COUNT);
+	for(int x = 0; x < MAP_WIDTH; x++)
+		for(int y = 0; y < MAP_HEIGHT; y++)
+			if(getTile(x, y).getTag() == "wall" && rand() % 20 == 0)
+				getTile(x, y).addDrawable(banner(rand() % banner::COUNT));
 }
--- src/dungeon/dungeon.h
+++ src/dungeon/dungeon.h
@@ -2,7 +2,9 @@
 #define DUNGEON_H
 
 #include <SFML/Graphics.hpp>
+#include <set>
 #include "../config.h"
+#include <iostream>
 
 class dungeon {
 public:
@@ -55,93 +57,108 @@ private:
 		}
 	};
 
-	struct tile {
-		enum TileType { FLOOR_T, WALL_T, EMPTY_T };
-		TileType type = EMPTY_T;
-		bool walkable = false;
+	struct drawable {
 		sf::Sprite sprite;
+		std::string tag;
+		int layer = 0;
 		int x = 0;
 		int y = 0;
-		tile(int x, int y, sf::IntRect textureRect) {
+
+		bool operator< (const drawable& obj) const {
+			return this->layer < obj.layer;
+		}
+
+		drawable(int x, int y, sf::IntRect textureRect) {
 			this->x = x;
 			this->y = y;
+			sprite.setPosition(x * config::TILE_SIZE,
+				y * config::TILE_SIZE + config::TILE_SIZE - sprite.getTextureRect().height);
 			sprite.setTexture(tileTexture);
 			sprite.setTextureRect(textureRect);
+		}
+		drawable(int x, int y, sf::IntRect textureRect, int layer) : drawable(x, y, textureRect) {
+			this->layer = layer;
+		}
+		drawable(sf::IntRect textureRect) {
+			sprite.setTexture(tileTexture);
+			sprite.setTextureRect(textureRect);
+		}
+		drawable(sf::IntRect textureRect, int layer) : drawable(textureRect) {
+			this->layer = layer;
+		}
+
+		void setPosition(int x, int y) {
+			this->x = x;
+			this->y = y;
 			sprite.setPosition(x * config::TILE_SIZE,
 				y * config::TILE_SIZE + config::TILE_SIZE - sprite.getTextureRect().height);
 		}
-		tile() {
-			sprite.setTexture(tileTexture);
+
+		void draw(sf::RenderWindow& window) const {
+			window.draw(sprite);
 		}
 
-		void draw(sf::RenderWindow& window) {
-			if(type != EMPTY_T)
-				window.draw(sprite);
+		void draw(int x, int y, sf::RenderWindow& window) {
+			sprite.setPosition(x * config::TILE_SIZE,
+				y * config::TILE_SIZE + config::TILE_SIZE - sprite.getTextureRect().height);
+			window.draw(sprite);
+			sprite.setPosition(this->x * config::TILE_SIZE,
+				this->y * config::TILE_SIZE + config::TILE_SIZE - sprite.getTextureRect().height);
 		}
 	};
-	struct wall : tile {
+
+	struct tile {
+		bool isEmpty() { return graphics.size() == 0; }
+		bool walkable = false;
+		void setWalkable(bool walkable) { this->walkable = walkable; }
+		int x = 0;
+		int y = 0;
+		std::set<drawable> graphics;
+		tile(int x, int y) {
+			this->x = x;
+			this->y = y;
+		}
+
+		void addDrawable(drawable item) {
+			item.setPosition(x, y);
+			graphics.insert(item);
+		}
+
+		void draw(sf::RenderWindow& window) const {
+			for(const drawable& graphic : graphics) {
+				//std::cout << "DRAW NIBBA" << std::endl;
+				graphic.draw(window);
+			}
+		}
+
+		std::string getTag() { return graphics.size() > 0 ? graphics.begin()->tag : ""; }
+	};
+
+	struct wall : drawable {
 		static const sf::IntRect WALL;
 		static const int COLUMNS = 7;
 		static const int COUNT = 7;
-		wall(int x, int y, int index) : tile(x, y,
-			sf::IntRect(WALL.left + WALL.width * (index % COLUMNS),
-			WALL.top + WALL.height * (index/COLUMNS), WALL.width, WALL.height)) {
-			type = WALL_T;
-		}
-		wall() : tile(0, 0, WALL) {
-			type = WALL_T;
+		wall(int index) : drawable(sf::IntRect(WALL.left + WALL.width * (index % COLUMNS),
+			WALL.top + WALL.height * (index/COLUMNS), WALL.width, WALL.height), 0) {
+			tag = "wall";
 		}
 	};
-	struct floor : tile {
+	struct floor : drawable {
 		static const sf::IntRect FLOOR;
 		static const int COLUMNS = 30;
 		static const int COUNT = 72;
-		floor(int x, int y, int index) : tile(x, y,
-			sf::IntRect(FLOOR.left + FLOOR.width * (index % COLUMNS),
-			FLOOR.top + FLOOR.height * (index/COLUMNS), FLOOR.width, FLOOR.height)) {
-			type = FLOOR_T;
-			walkable = true;
-		}
-		floor() : tile(0, 0, FLOOR) {
-			type = FLOOR_T;
-			walkable = true;
+		floor(int index) : drawable(sf::IntRect(FLOOR.left + FLOOR.width * (index % COLUMNS),
+			FLOOR.top + FLOOR.height * (index/COLUMNS), FLOOR.width, FLOOR.height), 0) {
+			tag = "floor";
 		}
 	};
-
-	struct decoration {
-		sf::Sprite sprite;
-		bool empty = true;
-		int x = 0;
-		int y = 0;
-		decoration(int x, int y, sf::IntRect textureRect) {
-			this->x = x;
-			this->y = y;
-			sprite.setTexture(tileTexture);
-			sprite.setTextureRect(textureRect);
-			sprite.setPosition(x * config::TILE_SIZE,
-				y * config::TILE_SIZE + config::TILE_SIZE - sprite.getTextureRect().height);
-		}
-		decoration() {
-			sprite.setTexture(tileTexture);
-		}
-
-		void draw(sf::RenderWindow& window) {
-			if(!empty)
-				window.draw(sprite);
-		}
-	};
-	struct banner : decoration {
+	struct banner : drawable {
 		static const sf::IntRect BANNER;
 		static const int COLUMNS = 6;
 		static const int COUNT = 6;
-		banner(int x, int y, int index) : decoration(x, y,
-			sf::IntRect(BANNER.left + BANNER.width * (index % COLUMNS),
-			BANNER.top + BANNER.height * (index/COLUMNS), BANNER.width, BANNER.height)) {
-			empty = false;
-		}
-
-		banner() : decoration(0 , 0, BANNER) {
-			empty = false;
+		banner(int index) : drawable(sf::IntRect(BANNER.left + BANNER.width * (index % COLUMNS),
+			BANNER.top + BANNER.height * (index/COLUMNS), BANNER.width, BANNER.height), 1) {
+			tag = "banner";
 		}
 	};
 
@@ -151,9 +168,7 @@ private:
 
 	//	TODO: Figure out why I can only use a vector and not an array.
 	std::vector<tile> map;
-	std::vector<decoration> decorationMap;
-	tile getTile(int x, int y);
-	decoration getDecoration(int x, int y);
+	tile& getTile(int x, int y);
 };
 
 #endif
