838068706c35c8f5990c6e30c558ea6c6aff78ce
William Fisher <william.fisher7v@stu.austinisd.org>
2018-04-03 09:49:50 -0500

    Started player

---
+++ src/player/player.cpp
@@ -0,0 +1,18 @@
+#include "player.h"
+
+player::player(PlayerType pClass, Gender gender) {
+	this->pClass = pClass;
+	this->gender = gender;
+}
+
+player::~player() {
+
+}
+
+void player::update(float deltaTime) {
+	animations[animState].update(deltaTime);
+}
+
+void draw(sf::RenderWindow& window) {
+	animations[animState].draw(window);
+}
---
+++ src/player/player.h
@@ -0,0 +1,33 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "../config.h"
+#include <SFML/Graphics.hpp>
+#include "../rendertools/animation.h"
+
+class player {
+public:
+	enum PlayerType { CLERIC, WARRIOR, RANGER, WIZARD, ROGUE };
+	enum Gender { MALE, FEMALE };
+	enum AnimationStates { IDLE = 0, GESTURE = 1, WALK = 2, ATTACK = 3, DEATH = 4 };
+
+	player(PlayerType pClass, Gender gender);
+	~player();
+
+	void update(float deltaTime);
+	void draw(sf::RenderWindow& window);
+
+	sf::Vector2i getPosition() { return position; }
+	void setPosition(sf::Vector2i position) { this->position = position; }
+
+	void setAnimState(AnimationStates animState) { this->animState = animState; }
+
+private:
+	sf::Vector2i position;
+	AnimationStates animState = IDLE;
+	PlayerType pClass = CLERIC;
+	Gender gender = ABSORBO;
+	animation animations[5];
+};
+
+#endif
